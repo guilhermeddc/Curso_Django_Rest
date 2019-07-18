711e73eaa736e210988360b5d49240083d4fa797
Guilherme <guilhermeddc@gmail.com>
2019-07-18 00:09:24 -0300

    refatorado nome do projeto

--- spots/urls.py
+++ spots/urls.py
@@ -1,4 +1,4 @@
-"""Curso_Django_Rest URL Configuration
+"""spots URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
